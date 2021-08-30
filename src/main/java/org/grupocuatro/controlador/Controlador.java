d26f08fbc1729b59e6a42816be646542b0ebf798
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-08-30 11:11:23 -0300

    carga archivos

---
+++ src/main/java/org/grupocuatro/controlador/Controlador.java
@@ -0,0 +1,34 @@
+package controlador;
+
+import java.util.Date;
+
+import modelo.Club;
+import modelo.Partido;
+
+public class Controlador {
+
+	public Controlador() { }
+	
+	/*
+	 * Notas:
+	 * 
+	 * -Pueden si lo desean convertir el controlador en un Singleton
+	 * 
+	 * -Deberan completar los metodos del controlador para que cumplan con los requerimientos
+	 *  del trabajo, Recuerden siempre aplicar los patrones GRASP para verificar la correcta 
+	 *  asignacion de lasresponsabilidades
+	 *  
+	 * -En la segunda parate del trabajo deber'an agragar los metodos y controles que 
+	 *  considen necesarios. */
+	public void modificarClub(String nombre, String direccion) { }
+	
+	public Integer agregarJugador(String documento, String nombre, int idClub, Date fechaNacimiento) { return null; }
+	
+	public void eliminarJugador(int idJugador, int idClub) { }
+	
+	public void habilitarJugador(int idJugador, int idClub, int idCampeonato) {	}
+	
+	public Integer crearListaJugadores(Club club, Partido partido) { return null;}	
+	
+	public void agregarJugadoresEnLista() {}
+}
---
+++ src/main/java/org/grupocuatro/modelo/Club.java
@@ -0,0 +1,69 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Club implements Comparable<Club>{
+
+	private Integer idClub;
+	private String nombre;
+	private String dirección;
+	private List<Responsable> responsables;
+	private List<Jugador> jugadores;
+	private List<Campeonato> participanciones;
+	
+	public Club(int idClub, String nombre, String dirección) {
+		this.idClub = idClub;
+		this.nombre = nombre;
+		this.dirección = dirección;
+		jugadores = new ArrayList<Jugador>();
+	}
+	
+	public void asignarResponsable(Responsable responsable) {
+		responsables.add(responsable);
+	}
+	
+	public void agregarJugador(Jugador jugador) {
+		jugadores.add(jugador);
+	}
+
+	public Integer getIdClub() {
+		return idClub;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDirección() {
+		return dirección;
+	}
+
+	public List<Responsable> getResponsable() {
+		return responsables;
+	}
+
+	public List<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	@Override
+	public int compareTo(Club o) {
+		return this.getIdClub().compareTo(o.getIdClub());
+	}
+	
+	public boolean participa(Campeonato campeonato) {
+		return participanciones.contains(campeonato);
+	}
+	public void participar(Campeonato campeonato) {
+		participanciones.add(campeonato);
+	}
+	
+	public void agregarJugadoresToListaLocal(Jugador jugador, Partido partido) {
+		partido.agregarJugadoresLocales(new Miembro(this, partido, jugador));
+	}
+	
+	public void agregarJugadoresToListaVisitante(Jugador jugador, Partido partido) {
+		partido.agregarJugadoresVisitantes(new Miembro(this, partido, jugador));
+	}
+}
