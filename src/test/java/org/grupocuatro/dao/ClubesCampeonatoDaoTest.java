cbbfda8d1f8ede8e500e0f89db8fe14b248fdea2
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2021-09-05 18:44:50 -0300

    ClubesCampeonatoDaoTest hecho

--- src/test/java/org/grupocuatro/dao/ClubesCampeonatoDaoTest.java
+++ src/test/java/org/grupocuatro/dao/ClubesCampeonatoDaoTest.java
@@ -1,15 +1,26 @@
 package org.grupocuatro.dao;
 
 import junit.framework.TestCase;
+import org.grupocuatro.excepciones.ClubesCampeonatoException;
 
 public class ClubesCampeonatoDaoTest extends TestCase {
 
-    public void testGetInstancia() {
-    }
-
     public void testGetClubesEnCampeonato() {
+        try {
+            System.out.println(ClubesCampeonatoDao.getInstancia().getClubesEnCampeonato(3));
+            System.out.println(ClubesCampeonatoDao.getInstancia().getClubesEnCampeonato(2));
+            System.out.println(ClubesCampeonatoDao.getInstancia().getClubesEnCampeonato(254));
+        } catch (ClubesCampeonatoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void testGetCampeonatosClub() {
+        try {
+            System.out.println(ClubesCampeonatoDao.getInstancia().getCampeonatosClub(1));
+            System.out.println(ClubesCampeonatoDao.getInstancia().getCampeonatosClub(14));
+        } catch (ClubesCampeonatoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
