6fccb82facf41f97ba0f0b23a45eac8dccd94052
agussturba <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>
2021-09-06 17:12:28 -0300

    GolesYFaltas

--- src/main/java/org/grupocuatro/controlador/Controlador.java
+++ src/main/java/org/grupocuatro/controlador/Controlador.java
@@ -6,6 +6,7 @@ import org.grupocuatro.modelo.*;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 public class Controlador {
 
@@ -143,6 +144,47 @@ public class Controlador {
         gol= new Gol(jugador,partido,minuto,tipo);
         goldao.save(gol);
         return gol.getIdGol();
+    }
+    public Integer cargarFalta(Integer idJugador, Integer idPartido,Integer idCameponato, int minuto, String tipo)  {
+        JugadorDao jugadordao = JugadorDao.getInstancia();
+        Jugador jugador= null;
+        try{
+            jugador=jugadordao.getJugadorById(idJugador);
+        }catch(JugadorException e){
+            System.out.println(e.getMessage());
+        }
+        PartidoDao partidodao= PartidoDao.getInstancia();
+        Partido partido=null;
+        try {
+            partido = partidodao.getInstancia().getPartidoById(idPartido);
+        }
+        catch (PartidoException e) {
+            e.printStackTrace();
+        }
+        CampeonatoDao campeonatoDao=CampeonatoDao.getInstancia();
+        Campeonato campeonato=null;
+        try{
+            campeonato=campeonatoDao.getCampeonato(idCameponato);
+
+        } catch (CampeonatoException e) {
+            e.printStackTrace();
+        }
+        FaltaDao faltadao=FaltaDao.getInstancia();
+        Falta falta =null;
+        falta= new Falta(jugador,partido,campeonato,minuto,tipo);
+        faltadao.save(falta);
+        List<Falta> lista_falta=null;
+        try{
+            lista_falta = faltadao.getFaltasByJugadorAndPartido(idJugador, idPartido);
+        }catch (FaltaException e){
+            System.out.println(e.getMessage());
+        }
+        int cantidad = lista_falta.size();
+        if (cantidad==2){
+            falta= new Falta(jugador,partido,campeonato,minuto,"Roja");
+            faltadao.save(falta);
+        }
+        return falta.getIdFalta();
     }
 
     // REVISAR ESTE METODO!
