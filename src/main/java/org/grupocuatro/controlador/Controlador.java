af9e5bae020717fa6fac53381cb6a7c3b38da3a5
Juli <julian_gamietea@hotmail.com>
2021-09-06 16:31:01 -0300

    cambios en responsable

--- src/main/java/org/grupocuatro/controlador/Controlador.java
+++ src/main/java/org/grupocuatro/controlador/Controlador.java
@@ -1,10 +1,7 @@
 package org.grupocuatro.controlador;
 
 import org.grupocuatro.dao.*;
-import org.grupocuatro.excepciones.CampeonatoException;
-import org.grupocuatro.excepciones.ClubException;
-import org.grupocuatro.excepciones.JugadorException;
-import org.grupocuatro.excepciones.MiembroException;
+import org.grupocuatro.excepciones.*;
 import org.grupocuatro.modelo.*;
 
 import java.time.LocalDate;
@@ -145,6 +142,44 @@ public class Controlador {
         }
 
     }
+
+    public Integer crearResponsable (String documento, String nombre, Integer idClub) throws ResponsableException {
+        ResponsableDao dao = ResponsableDao.getInstancia();
+        try{
+            Club club = ClubDao.getInstancia().getClubById(idClub);
+            try{
+                dao.getResponsableByNroDocAndClub(documento,idClub);
+                System.out.println("Ya existe el representante de DNI " + documento + " en el club " + idClub);
+            }catch (ResponsableException e){
+                Responsable r = new Responsable(documento,nombre,club);
+                r.save();
+                return r.getLegajo();
+            }
+        } catch (ClubException e) {
+            System.out.println(e.getMessage());
+        }
+        throw new ResponsableException("No se pudo crear el responsable solicitado");
+
+    }
+
+    public void modificarResponsable(Integer legajoResponsable, String documento, String nombre, Integer idClub){
+        ResponsableDao dao = ResponsableDao.getInstancia();
+        try{
+            Responsable resp = dao.getResponsable(legajoResponsable);
+            try{
+                Club club = ClubDao.getInstancia().getClubById(idClub);
+                resp.setClub(club);
+                resp.setNombre(nombre);
+                resp.setDocumento(documento);
+                resp.update();
+            }catch (ClubException c){
+                System.out.println(c.getMessage());
+            }
+        } catch (ResponsableException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
 
 }
